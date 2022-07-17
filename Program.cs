0e1a60ca5bdd6974342ee0f3f5642186290cfa80
Nathan Rodrigues dos Santos <97409681+NahNathan@users.noreply.github.com>
2022-07-17 17:30:18 -0300

    Leves melhorias
    Ainda não muito bom

--- Program.cs
+++ Program.cs
@@ -7,24 +7,34 @@
         //Começo
         Random r = new Random();
         Console.WriteLine("Bom dia, CyberDado Iniciado");
-        do{
-            Console.WriteLine("Digite o numero de faces do dado a ser rolado");
-            faces=System.Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Digite o numero do modificador");
-            mod=System.Convert.ToInt32(Console.ReadLine());
-            //Por enquanto só isso, vou fazer pra rolar mais de uma vez dps
-
-            Console.Clear();
-            rolada=r.Next(1, faces);
-            Console.WriteLine("Rolada: " + rolada);
-            Console.WriteLine("Modificador: " + mod);
-            Console.WriteLine("Total: " + (rolada+mod));
-            Console.WriteLine("Outra Rolada?");
-            Console.WriteLine("(Digite 1 para encerrar)");
-            selec=System.Convert.ToInt32(Console.ReadLine());
+        try{
+            do{
+               Console.WriteLine("Digite o numero de faces do dado a ser rolado");
+                faces=System.Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Digite o numero do modificador");
+                mod=System.Convert.ToInt32(Console.ReadLine());
+                //Por enquanto só isso, vou fazer pra rolar mais de uma vez dps
+                //Mostrando os resultados
+                Console.Clear();
+                rolada=r.Next(1, faces);
+                Console.WriteLine("Rolada: " + rolada);
+                Console.WriteLine("Modificador: " + mod);
+                Console.WriteLine("Total: " + (rolada+mod));
+                Console.WriteLine("Outra Rolada?");
+                Console.WriteLine("(Digite 1 para encerrar)");
+                selec=System.Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
             
-        }while(selec!=1);
+            }while(selec!=1);
+        }
+
+        catch(System.FormatException){
+            Console.WriteLine("Inválido, fechando dado");
+        }
         //Jeito bem BEM ruim de se fazer isso, eu sei
         //Corrijo outro dia, essa é só a prova de conceito msm
+
+        Console.WriteLine("Obrigado por usar esse programa");
+        Console.ReadLine();
     }
 }
