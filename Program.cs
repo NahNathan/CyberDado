41a04b6191a05c9d7d11d1ebe2e937095db5e5cf
Nathan Rodrigues dos Santos <97409681+NahNathan@users.noreply.github.com>
2022-07-20 15:52:53 -0300

    Adicionada função de rolar mais de 1 dado por vez
    E corrigido um bug q aparentemente era impossível rolar o máximo do dado

--- Program.cs
+++ Program.cs
@@ -2,37 +2,51 @@
     private static void Main(string[] args){
 
         //Declarando as variáveis
-        int faces=0, mod=0, rolada=0, selec=0;
+        int resp=0, faces=0, mod=0, rolada=0, num=0;
 
         //Começo
-        Random r = new Random();
+        Random r=new Random();
+        
+        Console.WriteLine("\n**==========================================**");
         Console.WriteLine("Bom dia, CyberDado Iniciado");
+        Console.WriteLine("Cuidado para digitar apenas números válidos \n");
         try{
             do{
-               Console.WriteLine("Digite o numero de faces do dado a ser rolado");
+                //Recebendo dados (hehe)
+                Console.WriteLine("Digite o número de dados a ser rolado");
+                num=System.Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Digite o número de faces do dado a ser rolado");
                 faces=System.Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Digite o numero do modificador");
+                Console.WriteLine("Digite o número do modificador");
                 mod=System.Convert.ToInt32(Console.ReadLine());
-                //Por enquanto só isso, vou fazer pra rolar mais de uma vez dps
-                //Mostrando os resultados
                 Console.Clear();
-                rolada=r.Next(1, faces);
-                Console.WriteLine("Rolada: " + rolada);
-                Console.WriteLine("Modificador: " + mod);
-                Console.WriteLine("Total: " + (rolada+mod));
-                Console.WriteLine("Outra Rolada?");
+                
+                //Mostrando os resultados
+                for (int i=1; i<=num; i++){
+                    rolada=r.Next(1, faces+1); //Rolando
+
+                    Console.Write("\nRolada numero " + i + " :         " + rolada);
+                    //Checando para ver se é um "crítico"
+                    if(rolada==faces)
+                        Console.Write("<--- Crítico!!\n");
+                    else
+                        Console.Write("\n"); //Isso é para mostrar na msm linha
+
+                    Console.WriteLine("Modificador numero " + i + " :    " + mod);
+                    Console.WriteLine("Total numero " + i + " :          "+ (rolada+mod));    
+                }
+                
+                Console.WriteLine("\nOutra Rolada?");
                 Console.WriteLine("(Digite 1 para encerrar)");
-                selec=System.Convert.ToInt32(Console.ReadLine());
+                resp=System.Convert.ToInt32(Console.ReadLine());
                 Console.Clear();
             
-            }while(selec!=1);
+            }while(resp!=1);
         }
 
         catch(System.FormatException){
-            Console.WriteLine("Inválido, fechando dado");
+            Console.WriteLine("Inválido, fechando programa");
         }
-        //Jeito bem BEM ruim de se fazer isso, eu sei
-        //Corrijo outro dia, essa é só a prova de conceito msm
 
         Console.WriteLine("Obrigado por usar esse programa");
         Console.ReadLine();
